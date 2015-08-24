0c3ec3703d03714a60ea1b24d4556bb6c13132a9
Arsen Gasparyan <to.arsen.gasparyan@gmail.com>
2015-08-24 23:00:34 +0300

    Add pg

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 var express = require('express');
 var app = express();
 var path = require("path");
+var pg = require('pg');
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+app.get('/go', function (req, res) {
+  res.sendFile(path.join(__dirname+'/go.html'));
+});
+
+
 var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
+});
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+    if (err) throw err;
+    console.log('Connected to postgres! Getting schemas...');
+
+    client.query("CREATE TABLE requests(phone VARCHAR(255), firstname VARCHAR(255), lastname VARCHAR(255));")
+
 });
