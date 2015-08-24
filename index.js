9c1d52aa1eb38d945cbd88a316a2fdfcbb3a9eb7
Arsen Gasparyan <to.arsen.gasparyan@gmail.com>
2015-08-24 23:24:34 +0300

    Add mongo

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 var express = require('express');
 var app = express();
 var path = require("path");
-var pg = require('pg');
+var MongoClient = require('mongodb').MongoClient;
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
+
+  MongoClient.connect(process.env.MONGOLAB_URI, function(err, db) {
+    var collection = db.collection('requests');
+    collection.insert({ first_name: "Arsen", last_name: "Gasparyan", phone: "999808630" }, function(err, result) {});
+
+    db.close();
+  });
 });
 
 app.get('/go', function (req, res) {
   res.sendFile(path.join(__dirname+'/go.html'));
-});
 
+});
 
 var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
-});
-
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-    if (err) throw err;
-    console.log('Connected to postgres! Getting schemas...');
-
-    client.query("CREATE TABLE requests(phone VARCHAR(255), firstname VARCHAR(255), lastname VARCHAR(255));")
-
 });
